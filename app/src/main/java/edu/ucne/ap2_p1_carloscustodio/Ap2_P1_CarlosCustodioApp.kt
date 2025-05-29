69dee92999af8504b07542117af068914c44691c
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-28 20:57:42 -0400

    hilt Injections

---
+++ app/src/main/java/edu/ucne/ap2_p1_carloscustodio/Ap2_P1_CarlosCustodioApp.kt
@@ -0,0 +1,7 @@
+package edu.ucne.ap2_p1_carloscustodio
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Ap2_P1_CarlosCustodioApp : Application()
