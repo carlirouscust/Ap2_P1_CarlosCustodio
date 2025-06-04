ba31abd8a9c114f81f9906aa8ef48ebc18baf3e2
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-04 19:37:23 -0400

    se completo el appmodule

--- app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/di/AppModule.kt
@@ -1,4 +1,31 @@
 package edu.ucne.ap2_p1_carloscustodio.data.di
 
+import android.content.Context
+import androidx.room.Room
+import edu.ucne.ap2_p1_carloscustodio.data.local.database.tareaDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideTareaDb(
+        @ApplicationContext appContext: Context
+    ): tareaDb {
+        return Room.databaseBuilder(
+            appContext,
+            tareaDb::class.java,
+            "tareaDb"
+        ).build()
+    }
+
+    @Provides
+    fun provideTareaDao(db: tareaDb) = db.tareaDao()
 }
