bcdd2df93355df2016e8beb9211056fd7d80d84d
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-04 19:20:10 -0400

    se creo el screen del navigation

--- app/src/main/java/edu/ucne/ap2_p1_carloscustodio/Presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/ap2_p1_carloscustodio/Presentation/navigation/Screen.kt
@@ -1,4 +1,11 @@
 package edu.ucne.ap2_p1_carloscustodio.Presentation.navigation
 
-class Screen {
+import edu.ucne.ap2_p1_carloscustodio.data.local.database.tareaDb
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object List : Screen()
+    @Serializable
+    data class tarea(val tareaDb: Int?) : Screen()
 }
