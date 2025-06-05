757c30a4f0380e23c48669ce05238078c6fd0370
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-04 21:18:44 -0400

    se completo el iustate

--- app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/sistema/TareaUiState.kt
+++ app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/sistema/TareaUiState.kt
@@ -5,7 +5,7 @@ import edu.ucne.ap2_p1_carloscustodio.data.local.entities.tareaEntity
 data class tareaUiState (
     val tareaid: Int? = null,
     val descripcion: String = "",
-    val tiempo: String,
+    val tiempo: String = "",
     val errorMessage: String? = null,
     val tareaEditandaId: Int? = null,
     val successMessage: String? = null,
