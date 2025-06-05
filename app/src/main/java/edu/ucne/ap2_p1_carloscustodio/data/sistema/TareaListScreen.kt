a730cdc2f508c721295e53be6c7d7569e531a003
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-04 20:27:12 -0400

    se creo el tarea list screen

--- app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/sistema/TareaListScreen.kt
+++ app/src/main/java/edu/ucne/ap2_p1_carloscustodio/data/sistema/TareaListScreen.kt
@@ -1,2 +1,130 @@
 package edu.ucne.ap2_p1_carloscustodio.data.sistema
 
+import android.icu.lang.UCharacter
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import  edu.ucne.ap2_p1_carloscustodio.data.local.entities.tareaEntity
+
+import android.icu.lang.UCharacter.NumericType
+
+
+@Composable
+fun TareaListScreen(
+    tareaList: List<tareaEntity>,
+    onCreate: () -> Unit,
+    onDelete: (tareaEntity) -> Unit,
+    onEdit: (tareaEntity) -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onCreate,
+                containerColor = Color(0xFF4CAF50),
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Agregar")
+            }
+        }
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color(0xFF9D9D9D), Color(0xFFEFF3F3))
+                    )
+                )
+                .padding(paddingValues)
+                .padding(horizontal = 18.dp, vertical = 18.dp)
+        ) {
+            Text(
+                text = "Lista de Sistema",
+                style = TextStyle(
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF202121),
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(39.dp))
+
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(18.dp)) {
+                items(tareaList) { tarea ->
+                    TareaRow(tarea = tarea, onDelete = onDelete, onEdit = onEdit)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TareaRow(
+    tarea: (tareaEntity),
+    onDelete: (tareaEntity) -> Unit,
+    onEdit: (tareaEntity) -> Unit
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(14.dp),
+        modifier = Modifier.fillMaxWidth(),
+
+        ) {
+        Row(
+            modifier = Modifier
+                .padding(22.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "Descripcion: ", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = tarea.descripcion, fontSize = 16.sp)
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "Tiempo: ", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = tarea.tiempo, fontSize = 16.sp)
+                }
+            }
+
+            Row {
+                IconButton(onClick = { onEdit(tarea) }) {
+                    Icon(Icons.Filled.Edit, contentDescription = "Editar", tint = Color(0xFF4CAF50))
+                }
+                IconButton(onClick = { onDelete(tarea) }) {
+                    Icon(Icons.Filled.Delete, contentDescription = "Eliminar", tint = Color.Red)
+                }
+            }
+        }
+    }
+}
